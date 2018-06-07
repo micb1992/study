238c9f28bc2b8e802cd22fb2103f67b1f9e703cd
micb1992 <13125323921@163.com>
2018-06-07 20:21:32 +0800

    配置文件

--- Demo/src/com/test/config/OperateConfig.java
+++ Demo/src/com/test/config/OperateConfig.java
@@ -15,24 +15,25 @@ import org.junit.Test;
 public class OperateConfig {
 	//读取properties配置文件
 	@Test
-	public void readProperties() throws IOException {
+	public String readProperties(String key) throws IOException {
 		Properties properties = new Properties();
 		FileInputStream inputStream = new FileInputStream(
 				"src/com/test/config/sys-config.properties");
 		properties.load(inputStream);
 		Iterator<String> iterator = properties.stringPropertyNames().iterator();
 		while (iterator.hasNext()) {
-			String key = iterator.next();
+			key = iterator.next();
 			System.out.println(key + ":" + properties.getProperty(key));
 		}
 		inputStream.close();
+		return properties.getProperty(key);
 	}
 //写入properties配置文件
 	@Test
-	public void writeProperties() throws Exception{
+	public void writeProperties(String key,String value) throws Exception{
 		Properties properties = new Properties();
 		FileOutputStream outputStream = new FileOutputStream(new File("src/com/test/config/sys-config.properties"), true);
-		properties.setProperty("IP", "192.168.0.1");
+		properties.setProperty(key, value);
 		properties.store(outputStream, "sys-config");
 		outputStream.close();
 	}
